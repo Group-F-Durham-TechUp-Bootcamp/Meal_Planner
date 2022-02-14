6690f6cd5b52501e373d0c6d54979d229c4d7987
lucybbbb <95537268+lucybbbb@users.noreply.github.com>
2022-02-14 07:38:19 +0000

    Update views.py

--- groupffit (Django sprint 1)/security/views.py
+++ groupffit (Django sprint 1)/security/views.py
@@ -4,9 +4,54 @@ from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required 
 
-def security(request):
-    return render(request, 'security/welcome.html', {'today': datetime.today()})
+from .form import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
-@login_required(login_url='/admin')
-def authorised(request):
-    return render(request, 'security/authorised.html', {})
+from django.contrib.auth.forms import AuthenticationForm
+
+
+#def security(request):
+   #return render(request, 'security/welcome.html', {'today': datetime.today()})
+
+#@login_required(login_url='/admin')
+#def authorised(request):
+   #return render(request, 'security/authorised.html', {})
+
+
+
+
+def login_user(request):
+    if request.method =="POST":
+       
+        form =NewUserForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, 
+            username= cd['username'],
+            password = cd['password'])
+
+            if user is not None:
+               login(requst, user)
+               return HttpResponse('authentification was successfull')
+
+            else:
+              return HttpResponse('authentification failed, please try again')
+    else:
+
+        form = NewUserForm()
+    return render(request=request, template_name='login.html',context= {'form':form})   
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("login.html")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name ="register.html", context={'register_form':form} )
+
